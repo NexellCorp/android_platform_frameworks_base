4a2f4610916cfb51deb993a9571428c508ad9b92
Sungwoo Park <swpark@nexell.co.kr>
2015-09-10 15:49:29 +0900

    [ADD] gson

---
+++ services/core/java/com/android/server/gson/annotations/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package provides annotations that can be used with {@link com.android.server.gson.Gson}.
+ * 
+ * @author Inderjeet Singh, Joel Leitch
+ */
+package com.android.server.gson.annotations;
